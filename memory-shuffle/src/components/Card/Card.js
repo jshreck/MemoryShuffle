868c8c6b9459884f22832abb6c4d5f1827017664
Julia Shreckhise <julia.shreckhise@gmail.com>
2018-06-11 19:14:22 -0400

    setting up

---
+++ memory-shuffle/src/components/Card/Card.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Card.css";
+
+const Card = props => (
+    <div className="card">
+      <div className="card-image">
+      <img class="responsive-img" src="https://upload.wikimedia.org/wikipedia/en/thumb/4/47/Spongebob-squarepants.svg/666px-Spongebob-squarepants.svg.png" alt=""/>
+      </div>
+    </div>
+  );
+  
+  export default Card;
