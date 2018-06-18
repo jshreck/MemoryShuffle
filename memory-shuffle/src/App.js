d8a09cbe7d16970e8c986d420b8c5bbd02bd63e4
Julia Shreckhise <julia.shreckhise@gmail.com>
2018-06-17 23:08:25 -0400

    working on onClick, no styling and needs images

--- memory-shuffle/src/App.js
+++ memory-shuffle/src/App.js
@@ -1,18 +1,92 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Card from "./components/Card";
+import cards from "./cards.json";
 
 class App extends Component {
+  state = {
+    score: 0,
+    cards: cards,
+  }
+
+  componentDidMount() {
+    this.start();
+  }
+
+  //to begin set score to 0 and all cards.clicked to false
+  start = () => {
+    this.setState({score:0});
+  
+    this.state.cards.forEach((card) => {
+      card.clicked = false;
+    });
+
+    this.setState({cards: cards});
+
+    this.shuffle();
+  }
+
+  //display cards in a random order
+  shuffle = () => {
+    let unshuffled = this.state.cards;
+    let shuffled = [];
+  
+
+   while (unshuffled.length > 0) {
+   let selected = Math.floor(Math.random() * unshuffled.length);
+   shuffled.push(unshuffled[selected]);
+   unshuffled.splice(selected, 1);
+   }
+
+   this.setState({cards: shuffled});
+  }
+
+  handleClick = (event) => {
+    let alreadyClicked = JSON.parse(event.target.getAttribute("data-clicked"));
+    console.log(typeof alreadyClicked);
+    console.log("id = " + event.target.getAttribute("id"));
+
+    if (alreadyClicked) {
+      //display some sort of message...
+      console.log("already clickd");
+      // this.start();
+    }
+    else {
+
+      console.log("not clicked");
+     let id = event.target.getAttribute("id");
+     let update = this.state.cards.filter(card => card.id === id);
+    console.log(update); //NOT WORKING
+    
+    this.setState({score: this.state.score + 1, cards: update});
+    console.log(this.state);
+
+    }
+   
+    //if clicked then restart game score = 0
+    //if not clicked, up score and set to clicked
+    //after processing, then either do display or start
+  }
+
+
+
+
   render() {
     return (
-      
+  
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Score: {this.state.score}</h1>
         </header>
-        <Card/>
+        {this.state.cards.map((card) => (
+            <Card
+              id={card.id}
+              key={card.id}
+              image={card.image}
+              clicked={card.clicked}
+              onClick={this.handleClick}
+            />
+        ))}
       </div>
     );
   }
--- memory-shuffle/src/components/Card/Card.js
+++ memory-shuffle/src/components/Card/Card.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./Card.css";
 
 const Card = props => (
-    <div className="card">
-      <div className="card-image">
-      <img class="responsive-img" src="https://upload.wikimedia.org/wikipedia/en/thumb/4/47/Spongebob-squarepants.svg/666px-Spongebob-squarepants.svg.png" alt=""/>
-      </div>
-    </div>
-  );
   
-  export default Card;
+    <div className="col-xs-6 col-md-3">
+      <a className="thumbnail">
+        <img src={props.image} alt="" id={props.id} onClick={props.onClick} data-clicked={props.clicked}/>
+      </a>
+    </div>
+
+);
+
+export default Card;
